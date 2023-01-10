014faa78508c883a7967bcef9c4a63fa1f6a65c5
AngelHerreraZe <angelherrera1996@gmail.com>
2023-01-09 19:10:57 -0600

    Search bar added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import SearchBar from './components/SearchBar';
 function App() {
   const [location, setLocation] = useState({});
   const [residents, setResidents] = useState([]);
+  const [searchId, setSearchId] = useState("");
 
   useEffect(() => {
     const randomId = Math.floor(Math.random() * 126) + 1;
@@ -20,11 +21,31 @@ function App() {
       });
   },[])
 
+  const searchLocation = () => {
+
+    if (searchId < 1 || searchId > 126){
+      alert("ID cannot be greater than 126");
+    } else{
+        axios
+        .get(`https://rickandmortyapi.com/api/location/${searchId}`)
+        .then(res => {setLocation(res.data)
+                      setResidents(res.data.residents)
+        });
+    }
+  }
+
   return (
     <div className="App">
       <Header />
       <div className='body'>
         <Location location={location}/>
+        <input
+          type="text"
+          placeholder="type a location id"
+          value={searchId}
+          onChange={(e) => setSearchId(e.target.value)}
+        />
+        <button onClick={searchLocation}>Search location</button>
         <div className='residents-body'>
           {
             residents.map(resident =>(
